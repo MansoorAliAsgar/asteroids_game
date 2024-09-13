abd30897aba65f327dae1b140912ef347d9a307c
MansoorAliAsgar <mansoor.aa1097@gmail.com>
2024-09-14 03:28:38 +0530

    add fix for pygame init

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
